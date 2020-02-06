b489f9e5de308f28f0cc97d93a493e6c5d77ebdd
shaffy-aggarwal-vst-au4 <54498198+shaffy-aggarwal-vst-au4@users.noreply.github.com>
2020-02-06 05:37:08 +0100

    Add files via upload

--- arraymenu/arraymenu.js
+++ arraymenu/arraymenu.js
@@ -1,1 +1,15 @@
+var menu=['home',"sevices",'about','contact'];
 
+var container = document.createElement('div');
+ document.body.appendChild(container);
+ container.setAttribute('id','container');
+ var x=menu.length;
+for(x=0;x<menu.length;x++)
+{
+ var link=document.createElement("a");
+ container.appendChild(link);
+ link.setAttribute('href',menu[x]+".html");
+ link.setAttribute("target","_blank");
+ var text=document.createTextNode(menu[x]);
+ link.appendChild(text);
+}
